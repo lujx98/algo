f0ee33c45c25e75379552a3039d200d09c5d7ebf
Lu <lujx98@gmail.com>
2022-04-17 17:37:16 +0800

    feat#algo:q226

---
+++ src/main/java/com/lu/algo/tree/q226.java
@@ -0,0 +1,59 @@
+package com.lu.algo.tree;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * @author sheldon
+ * @date 2022-04-06
+ */
+public class q226 {
+
+    public class TreeNode{
+        public Integer val;
+        public TreeNode right;
+        public TreeNode left;
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null){
+            return root;
+        }
+        TreeNode temp = null;
+        temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    public TreeNode invertTreeDFS(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+
+        }
+    }
+
+    public TreeNode invertTreeBFS(TreeNode root){
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        if (root == null) {
+            return root;
+        }
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode poll = queue.poll();
+            TreeNode temp = poll.right;
+            poll.right = poll.left;
+            poll.left = temp;
+            if (poll.left != null) {
+                queue.offer(poll.left);
+            }
+            if (poll.right != null) {
+                queue.offer(poll.right);
+            }
+        }
+        return root;
+    }
+}

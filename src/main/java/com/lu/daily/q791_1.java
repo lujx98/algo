f33e9c86426ef9481c30f26539d4ed9858d4493a
Administrator <1>
2022-11-13 18:01:53 +0800

    feat#algo

---
+++ src/main/java/com/lu/daily/q791_1.java
@@ -0,0 +1,48 @@
+package com.lu.daily;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author sheldon
+ * @date 2022-11-13
+ */
+public class q791_1 {
+
+    @Test
+    public void test(){
+        System.out.println(customSortString("cba","abcd"));
+    }
+
+    public String customSortString(String order, String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < order.toCharArray().length; i++) {
+            map.put(order.charAt(i), order.length() - i);
+        }
+        char[] a = s.toCharArray();
+
+        Character[] arr = new Character[s.length()];
+        for (int i = 0; i < s.length(); ++i) {
+            arr[i] = s.charAt(i);
+        }
+
+        Arrays.sort(arr, (v1, v2) -> {
+            Integer v1s = map.get(v1);
+            Integer v2s = map.get(v2);
+            v1s = v1s == null ? 0 : v1s;
+            v2s = v2s == null ? 0 : v2s;
+            return v2s - v1s;
+        });
+
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < s.length(); ++i) {
+            ans.append(arr[i]);
+        }
+        return ans.toString();
+    }
+
+}

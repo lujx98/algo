935af9f61025e7dee6f957f31a82831a83799aae
Lu <lujx98@gmail.com>
2022-11-24 22:56:06 +0800

    feat#algo

---
+++ src/main/java/com/lu/daily/q795_1.java
@@ -0,0 +1,13 @@
+package com.lu.daily;
+
+/**
+ * @author sheldon
+ * @date 2022-11-24
+ */
+public class q795_1 {
+
+    public int numSubarrayBoundedMax(int[] nums, int left, int right) {
+        return 0;
+    }
+
+}
--- src/main/java/com/lu/dmsxl/dp/DP_41_q300_0_LengthOfLIS.java
+++ src/main/java/com/lu/dmsxl/dp/DP_41_q300_0_LengthOfLIS.java
@@ -9,12 +9,28 @@ import java.util.function.Function;
  * @author sheldon
  * @date 2022-10-04
  */
-public class DP_q300_0_LengthOfLIS {
+public class DP_41_q300_0_LengthOfLIS {
 
     @Test
     public void test() {
-        Function<String, String> function = (string) -> string;
-        System.out.println(function.apply("dwaasd"));
+        System.out.println(lengthOfLIS1(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
+    }
+
+    public int lengthOfLIS1(int[] nums) {
+        int[] dp = new int[nums.length];
+        //dp[i] 表示第i位为止最大的子序列
+        Arrays.fill(dp, 1);
+        int res = 0;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            res = Math.max(res, dp[i]);
+        }
+
+        return res;
     }
 
     public int lengthOfLIS(int[] nums) {

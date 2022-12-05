1e85f9e82045ae3262649965938995a9940493ca
sheldon <sheldon.lu@kreditme.id>
2022-12-05 09:59:54 +0800

    feat#algo:每日一题

---
+++ src/main/java/com/lu/daily/q1687_1_动态规划单调队列.java
@@ -0,0 +1,16 @@
+package com.lu.daily;
+
+/**
+ * @author sheldon
+ * @date 2022-12-05
+ */
+public class q1687_1_动态规划单调队列 {
+
+    public int boxDelivering(int[][] boxes, int portsCount, int maxBoxes, int maxWeight) {
+        //dp[boxes+1][portsCount+2] = 最少次数
+
+
+        return 0;
+    }
+
+}

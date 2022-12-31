08f54dbb1090aac5fc1d30b3cd54b57248eeb480
Lu <lujx98@gmail.com>
2022-12-31 18:53:42 +0800

    feat#algo:每日一题

---
+++ src/main/java/com/lu/daily/q2037_贪心_排序.java
@@ -0,0 +1,29 @@
+package com.lu.daily;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author sheldon
+ * @date 2022-12-31
+ */
+public class q2037_贪心_排序 {
+
+    @Test
+    public void test(){
+        System.out.println(minMovesToSeat(new int[]{3,1,5}, new int[]{2,7,4}));
+    }
+
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int res = 0;
+        for (int i = 0; i < seats.length; i++) {
+            res += Math.abs(students[i]-seats[i]);
+        }
+
+        return res;
+    }
+
+}

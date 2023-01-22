d69a42349d95c0f2cba9a1b1df946b3f48e28189
Lu <lujx98@gmail.com>
2023-01-23 00:49:56 +0800

    feat#algo:每日一题

---
+++ src/main/java/com/lu/daily/q2303_模拟.java
@@ -0,0 +1,34 @@
+package com.lu.daily;
+
+import com.lu.structure.ArraysUtils;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author sheldon
+ * @date 2023-01-23
+ */
+public class q2303_模拟 {
+
+    @Test
+    public void test(){
+        System.out.println(calculateTax(ArraysUtils.makeIntArraysByString("[[3,50],[7,10],[12,25]]"), 10));
+    }
+
+    public double calculateTax(int[][] brackets, int income) {
+        double res = 0.0;
+        int cut = 0;
+        for (int[] bracket : brackets) {
+            if (income < bracket[0]) {
+                res += (income - cut) * (double) bracket[1] / 100;
+                break;
+            } else {
+                res += (bracket[0] - cut) * (double) bracket[1] / 100;
+            }
+            cut = bracket[0];
+        }
+        return res;
+    }
+
+}

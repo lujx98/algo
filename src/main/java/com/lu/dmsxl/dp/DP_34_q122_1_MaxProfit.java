3ee2fd7d32df12f4e2e4c4a2b4c9700c59760bd1
sheldon <sheldon.lu@kreditme.id>
2023-01-30 12:55:11 +0800

    feat#algo:动态规划

--- src/main/java/com/lu/dmsxl/dp/DP_34_q122_1_MaxProfit.java
+++ src/main/java/com/lu/dmsxl/dp/DP_34_q122_1_MaxProfit.java
@@ -10,7 +10,20 @@ public class DP_34_q122_1_MaxProfit {
 
     @Test
     public void test() {
-        System.out.println(maxProfit1(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(maxProfit2(new int[]{7, 1, 5, 3, 6, 4}));
+    }
+
+    public int maxProfit2(int[] prices) {
+        int[][] dp = new int[prices.length][2];
+        //dp[i][1] = max(dp[i-1][1], dp[i-1][0] -prices[i])
+        //dp[i][0] = max(dp[i-1][0], dp[i-1][1] +prices[i])
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] -prices[i]);
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] +prices[i]);
+        }
+        return dp[prices.length-1][0];
     }
 
     public int maxProfit1(int[] prices) {

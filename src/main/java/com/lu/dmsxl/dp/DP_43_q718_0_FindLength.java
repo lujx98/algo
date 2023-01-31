cb625582d5aa3b8dc501e8c2d1e66ffbf73905e1
sheldon <sheldon.lu@kreditme.id>
2023-01-31 10:51:47 +0800

    feat#algo:lis复习

--- src/main/java/com/lu/dmsxl/dp/DP_43_q718_0_FindLength.java
+++ src/main/java/com/lu/dmsxl/dp/DP_43_q718_0_FindLength.java
@@ -2,8 +2,6 @@ package com.lu.dmsxl.dp;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-
 /**
  * @author sheldon
  * @date 2022-10-04
@@ -12,7 +10,25 @@ public class DP_43_q718_0_FindLength {
 
     @Test
     public void test() {
-        System.out.println(findLength2(new int[]{1, 0, 0, 0, 1}, new int[]{1, 0, 0, 1, 1}));
+        System.out.println(findLength1(new int[]{1, 2, 3, 2, 1}, new int[]{3, 2, 1, 4, 7}));
+    }
+
+    public int findLength3(int[] nums1, int[] nums2) {
+        //dp[i][j] 表示到nums[i] nums[j]为止，之前最大公共序列长度
+        //dp[i][j] = 0 如果nums1[i] nums2[j]不等
+        //dp[i][j] = max(dp[i][j],dp[i-1][j-1]+1) 如果nums1[i] nums2[j]等
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+        int res = 0;
+        for (int i = 1; i <= nums1.length; i++) {
+            for (int j = 1; j <= nums2.length; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    if ((dp[i][j] = dp[i - 1][j - 1] + 1) > res) {
+                        res = dp[i][j];
+                    }
+                }
+            }
+        }
+        return res;
     }
 
     public int findLength2(int[] nums1, int[] nums2) {

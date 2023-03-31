d0052cd000959aa4061911a3f3ba21005d6cfde3
developer-01 <developer-01@kredito.id>
2023-03-31 11:27:18 +0800

    feat#algo

--- src/main/java/com/lu/dmsxl/dp/DP_20_q474_1_FindMaxForm.java
+++ src/main/java/com/lu/dmsxl/dp/DP_20_q474_1_FindMaxForm.java
@@ -4,28 +4,33 @@ package com.lu.dmsxl.dp;
  * @author sheldon
  * @date 2022-08-25
  */
-public class DP_q474_1_FindMaxForm {
+public class DP_20_q474_1_FindMaxForm {
 
     public int findMaxForm(String[] strs, int m, int n) {
-        int[][] dp = new int[m + 1][n + 1];
-        for (String str : strs) {
-            int oneNumber = 0;
-            int zeroNumber = 0;
-            for (char c : str.toCharArray()) {
-                if (c == '0') {
-                    zeroNumber++;
-                } else {
-                    oneNumber++;
-                }
-            }
-            for (int i = m; i >= zeroNumber; i--) {
-                for (int j = n; j >= oneNumber; j--) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i - zeroNumber][j - oneNumber] + 1);
-                }
-            }
-        }
-        return dp[m][n];
+
+        return 0;
     }
+
+//    public int findMaxForm(String[] strs, int m, int n) {
+//        int[][] dp = new int[m + 1][n + 1];
+//        for (String str : strs) {
+//            int oneNumber = 0;
+//            int zeroNumber = 0;
+//            for (char c : str.toCharArray()) {
+//                if (c == '0') {
+//                    zeroNumber++;
+//                } else {
+//                    oneNumber++;
+//                }
+//            }
+//            for (int i = m; i >= zeroNumber; i--) {
+//                for (int j = n; j >= oneNumber; j--) {
+//                    dp[i][j] = Math.max(dp[i][j], dp[i - zeroNumber][j - oneNumber] + 1);
+//                }
+//            }
+//        }
+//        return dp[m][n];
+//    }
 
 //    public int findMaxForm1(String[] strs, int m, int n) {
 //
--- src/main/java/com/lu/dmsxl/dp/动态规划_q494_目标和.java
+++ src/main/java/com/lu/dmsxl/dp/动态规划_q494_目标和.java
@@ -1,17 +1,37 @@
 package com.lu.dmsxl.dp;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
 /**
  * @author sheldon
  * @date 2022-06-05
  */
-public class DP_16_q494_1_FindTargetSumWays {
+public class 动态规划_q494_目标和 {
+
+    @Test
+    public void test() {
+        System.out.println(findTargetSumWays(new int[]{100, 100}, -400));
+    }
 
     public int findTargetSumWays(int[] nums, int target) {
+        int count = 0;
+        for (int num : nums) {
+            count += num;
+        }
+        if ((count + target) % 2 != 0||Math.abs(count) < target) {
+            return 0;
+        }
+        target = Math.abs(count + target) / 2;
 
-
-
-
-        return 0;
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = target; j >= nums[i]; j--) {
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+        return dp[target];
     }
 
 //    public int findTargetSumWays(int[] nums, int target) {

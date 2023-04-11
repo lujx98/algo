0fd6ff406bb7097455a5828959c4e38b86f369b9
Administrator <1>
2023-04-11 21:45:25 +0800

    feat#algo

---
+++ algo-all/src/main/java/com/lu/all/模拟_q1041_困于环中的机器人.java
@@ -0,0 +1,17 @@
+package com.lu.all;
+
+/**
+ * @author sheldon
+ * @date 2023-04-11
+ */
+public class 模拟_q1041_困于环中的机器人 {
+
+    public boolean isRobotBounded(String instructions) {
+        boolean x = false, y = true;
+        int[] ints = new int[2];
+
+
+        return false;
+    }
+
+}

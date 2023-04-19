0b07d0085e369bf2aac2d525c50042f591980187
developer-01 <developer-01@kredito.id>
2023-04-19 13:51:21 +0800

    feat#algo

---
+++ algo-all/src/main/java/com/lu/all/rev_q1023.java
@@ -0,0 +1,37 @@
+package com.lu.all;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author sheldon
+ * @date 2023-04-14
+ */
+public class rev_q1023 {
+
+    public List<Boolean> camelMatch(String[] queries, String pattern) {
+//        List<Boolean> res = new ArrayList<>();
+//        for (String query : queries) {
+//            char[] chars = pattern.toCharArray();
+//            int index = 0;
+//            boolean done = false;
+//            char[] querys = query.toCharArray();
+//            int i = 0;
+//            for (; i < querys.length && index < chars.length; i++) {
+//                if (querys[i] == chars[index]) {
+//                    index++;
+//                }
+//            }
+////            if (index == chars.length &&i!=) {
+////
+////            }
+//            //query遍历完  pattern没遍历完：false
+//            if (index != chars.length - 1) {
+//                res.add(false);
+//            }
+//        }
+//        return res;
+        return null;
+    }
+
+}
--- algo-all/src/main/java/com/lu/all/rev_q1040.java
+++ algo-all/src/main/java/com/lu/all/rev_q1040.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  * @author sheldon
  * @date 2023-04-07
  */
-public class q1040_ {
+public class rev_q1040 {
 
     public int[] numMovesStonesII(int[] stones) {
         Arrays.sort(stones);

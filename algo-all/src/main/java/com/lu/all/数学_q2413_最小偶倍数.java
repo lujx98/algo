183398401c45fb965164139f6282135fa9284e5d
developer-01 <developer-01@kredito.id>
2023-04-21 14:11:16 +0800

    feat#algo:

--- algo-all/src/main/java/com/lu/all/数学_q2413_最小偶倍数.java
+++ algo-all/src/main/java/com/lu/all/数学_q2413_最小偶倍数.java
@@ -10,6 +10,7 @@ public class 数学_q2413_最小偶倍数 {
         if(n==1){
             return 2;
         }
+        //看到if else并且数据是随机的，应该要防止分支预测
         if(n%2==0){
             return n;
         }else{

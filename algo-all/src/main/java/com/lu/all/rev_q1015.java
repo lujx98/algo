1fa60f00ec3c81510e394779081010cadbef1a0a
developer-01 <developer-01@kredito.id>
2023-05-15 09:57:53 +0800

    feat#algo

---
+++ algo-all/src/main/java/com/lu/all/rev_q1015.java
@@ -0,0 +1,14 @@
+package com.lu.all;
+
+/**
+ * @author sheldon
+ * @date 2023-05-10
+ */
+public class rev_q1015 {
+
+    public int smallestRepunitDivByK(int k) {
+
+        return 0;
+    }
+
+}
---
+++ algo-all/src/main/java/com/lu/all/rev_q1016.java
@@ -0,0 +1,16 @@
+package com.lu.all;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author sheldon
+ * @date 2023-05-11
+ */
+public class rev_q1016 {
+
+    public boolean queryString(String s, int n) {
+        return true;
+    }
+
+}

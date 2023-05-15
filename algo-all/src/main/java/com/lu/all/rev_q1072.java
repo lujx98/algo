35afbcc6ecbebe084d764f8ffbb3d4a267e0d2e2
developer-01 <developer-01@kredito.id>
2023-05-15 18:21:44 +0800

    feat#algo

---
+++ algo-all/src/main/java/com/lu/all/rev_q1072.java
@@ -0,0 +1,55 @@
+package com.lu.all;
+
+import com.lu.utils.ArraysUtils;
+import org.junit.Test;
+
+import javax.crypto.IllegalBlockSizeException;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.HashMap;
+
+/**
+ * @author sheldon
+ * @date 2023-05-15
+ */
+public class rev_q1072 {
+
+    @Test
+    public void test() {
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+
+//        System.out.println(maxEqualRowsAfterFlips(ArraysUtils.makeIntArraysByString("[[0,1],[1,1]]")));
+    }
+
+
+
+//    public int maxEqualRowsAfterFlips(int[][] matrix) {
+//        BigInteger[] ints = new BigInteger[matrix.length];
+//        for (int i = 0; i < matrix.length; i++) {
+//            StringBuilder stringBuilder = new StringBuilder();
+//            for (int matrix1 : matrix[i]) {
+//                stringBuilder.append(matrix1);
+//            }
+//            ints[i] = new BigInteger(stringBuilder.toString(), 2);
+//        }
+//
+//        int res = 1;
+//        BigInteger the = BigInteger.ONE.shiftLeft(matrix[0].length).subtract(BigInteger.ONE);
+//        for (int i = 0; i < ints.length; i++) {
+//            int cur = 1;
+//            for (int j = 0; j < ints.length; j++) {
+//                if (j == i) {
+//                    continue;
+//                }
+//                if (((ints[i].equals(ints[j]))) || ((ints[i].or(ints[j]).equals(the)) && ((ints[i].and(ints[j]).equals(BigInteger.ZERO))))) {
+//                    cur++;
+//                }
+//            }
+//            res = Math.max(res, cur);
+//        }
+//
+//        return res;
+//    }
+
+}

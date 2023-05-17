2db7742f496c6c3cdc421951dda22ddebee137ef
developer-104 <developer-104@kredito.id>
2023-05-17 10:06:52 +0800

    feat#algo

---
+++ algo-all/src/main/java/com/lu/all/模拟_q2446_判断两个事件是否存在冲突.java
@@ -0,0 +1,16 @@
+package com.lu.all;
+
+/**
+ * @author sheldon
+ * @date 2023-05-17
+ */
+public class 模拟_q2446_判断两个事件是否存在冲突 {
+
+    public boolean haveConflict(String[] event1, String[] event2) {
+
+
+
+        return false;
+    }
+
+}

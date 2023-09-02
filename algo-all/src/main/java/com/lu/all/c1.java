f9cd9eb0ebc98da864bafa58105f312ebf132fd1
Administrator <1>
2023-09-02 22:20:29 +0800

    feat#algo

---
+++ algo-all/src/main/java/com/lu/all/c1.java
@@ -0,0 +1,18 @@
+package com.lu.all;
+
+import org.junit.Test;
+
+/**
+ * @author sheldon
+ * @date 2023-08-27
+ */
+public class c1 {
+
+    @Test
+    public void test() {
+        System.out.println();
+    }
+
+
+
+}
---
+++ algo-all/src/main/java/com/lu/all/c4.java
@@ -0,0 +1,18 @@
+package com.lu.all;
+
+import org.junit.Test;
+
+/**
+ * @author sheldon
+ * @date 2023-09-02
+ */
+public class c4 {
+
+    @Test
+    public void test() {
+        System.out.println();
+    }
+
+
+
+}

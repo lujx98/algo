de3ca6ad7ab69daa5e2ea2337ae76dc668eeaefa
Administrator <1>
2023-09-10 12:19:26 +0800

    feat#algo:c1

--- algo-all/src/main/java/com/lu/contest/c1.java
+++ algo-all/src/main/java/com/lu/contest/c1.java
@@ -2,6 +2,9 @@ package com.lu.contest;
 
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.List;
+
 /**
  * @author sheldon
  * @date 2023-09-02
@@ -13,6 +16,14 @@ public class c1 {
         System.out.println();
     }
 
-
+    public int numberOfPoints(List<List<Integer>> nums) {
+        HashSet<Integer> res = new HashSet<>();
+        for (List<Integer> num : nums) {
+            for (Integer i = num.get(0); i <= num.get(1); i++) {
+                res.add(i);
+            }
+        }
+        return res.size();
+    }
 
 }
